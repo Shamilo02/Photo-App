9256f11585801e594ade20c1d2ec42eeee5c782d
Shamil Gurbanov <87391176+Shamilo02@users.noreply.github.com>
2022-08-13 15:41:20 -0700

    new

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ import express from "express";
 const app = express();
 const port = 5000; 
 
+//* middleware
+app.use(express.static('public'))
+
+
 app.get("/", (req,res) => {
     res.send("Index sehifesi")
 })
+
 
 
 app.listen(port, ()=> {
